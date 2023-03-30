472c22a0606b6f709a2e85e67aa089271c8a08aa
Adam Kraus <adamkraus6@gmail.com>
2023-03-30 17:37:09 -0600

    project creation

---
+++ app/src/main/java/edu/sdsmt/Hamster_Run_Kraus_Adam/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.sdsmt.Hamster_Run_Kraus_Adam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
