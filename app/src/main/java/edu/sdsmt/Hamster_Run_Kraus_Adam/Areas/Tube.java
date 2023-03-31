a1e6d9421d8d4b8ce2e61638a657e1cf4266eea2
Adam Kraus <adamkraus6@gmail.com>
2023-03-31 11:06:20 -0600

    game area stuff

---
+++ app/src/main/java/edu/sdsmt/Hamster_Run_Kraus_Adam/Areas/Tube.java
@@ -0,0 +1,19 @@
+package edu.sdsmt.Hamster_Run_Kraus_Adam.Areas;
+
+public class Tube {
+    public void move() {
+
+    }
+
+    public void enter() {
+
+    }
+
+    public void pickup() {
+
+    }
+
+    public void exit() {
+
+    }
+}
---
+++ app/src/main/java/edu/sdsmt/Hamster_Run_Kraus_Adam/Areas/Zoom.java
@@ -0,0 +1,23 @@
+package edu.sdsmt.Hamster_Run_Kraus_Adam.Areas;
+
+public class Zoom extends Tube {
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void enter() {
+
+    }
+
+    @Override
+    public void pickup() {
+
+    }
+
+    @Override
+    public void exit() {
+
+    }
+}
